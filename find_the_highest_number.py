5974bd510faa55eb594f993df49f36c55467da46
mjcarant0 <marjoymcaranto@iskolarngbayan.pup.edu.ph>
2024-10-14 03:50:25 +0800

    Compare the variables using functions

--- find_the_highest_number.py
+++ find_the_highest_number.py
@@ -1,7 +1,18 @@
 #Use def to identify the user's highest number
 def highest_number(var1, var2, var3, var4, var5):
-#Compare all the variables
-
+    #Compare all the variables
+    if var1 > var2 and var1 > var3 and var1 > var4 and var1 > var5:
+        return var1
+    elif var2 > var1 and var2 > var3 and var2 > var4 and var2 > var5:
+        return var2
+    elif var3 > var1 and var3 > var2 and var3 > var4 and var3 > var5:
+        return var3
+    elif var4 > var1 and var4 > var2 and var4 > var3 and var4 > var5:
+        return var4
+    elif var5 > var1 and var5 > var2 and var5 > var3 and var1 > var4:
+        return var5
+    else:
+        return "Error"
 #Create a variable to store the user's 5 numbers
 
 #Create a variable for the result
