f4f30b4c9617ab99a8f45b5767a78c03c1c6b1b6
mjcarant0 <marjoymcaranto@iskolarngbayan.pup.edu.ph>
2024-10-14 03:36:40 +0800

    Pseudocode

---
+++ find_the_highest_number.py
@@ -0,0 +1,9 @@
+#Use def to identify the user's highest number
+
+#Compare all the variables
+
+#Create a variable to store the user's 5 numbers
+
+#Create a variable for the result
+
+#Print the result of finding the highest number
